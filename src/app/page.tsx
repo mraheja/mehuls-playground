b7ae694a92b35c6dbb3c1d21060db77a961db2cf
mraheja <mraheja@berkeley.edu>
2024-07-03 02:12:39 -0700

    fix homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { PAGES } from "../constant/pages";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-muted">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-y-3 p-24 bg-muted">
       <a href={PAGES.EYE_TRACKER}>
         <Button>Eye Tracker</Button>
       </a>
