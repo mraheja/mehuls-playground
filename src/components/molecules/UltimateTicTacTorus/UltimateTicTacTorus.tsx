28ac1c0b2f025bb953fa7f7acb1706b68f2bd146
mraheja <mraheja@berkeley.edu>
2025-02-01 22:41:21 -0800

    animate in

--- src/components/molecules/UltimateTicTacTorus/UltimateTicTacTorus.tsx
+++ src/components/molecules/UltimateTicTacTorus/UltimateTicTacTorus.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { TicTacToe } from "../TicTacToe/TicTacToe";
+import { useState, useEffect } from "react";
 
 export interface UltimateTicTacTorusProps {
   className?: string;
@@ -7,6 +10,15 @@ export interface UltimateTicTacTorusProps {
 export const UltimateTicTacTorus: React.FC<UltimateTicTacTorusProps> = ({
   className,
 }) => {
+  const [visibleBoards, setVisibleBoards] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setVisibleBoards((prev) => (prev < 8 ? prev + 1 : prev));
+    }, 200);
+    return () => clearInterval(timer);
+  }, []);
+
   const board = (
     <div className="grid grid-cols-3 gap-3 w-[500px] place-items-center">
       {Array(9)
@@ -19,18 +31,30 @@ export const UltimateTicTacTorus: React.FC<UltimateTicTacTorusProps> = ({
     </div>
   );
 
+  const absoluteBoards = [
+    "top-0 left-full translate-x-3",
+    "top-0 right-full -translate-x-3",
+    "bottom-full left-0 -translate-y-3",
+    "top-full left-0 translate-y-3",
+    "top-full right-full translate-y-3 -translate-x-3",
+    "bottom-full right-full -translate-y-3 -translate-x-3",
+    "top-full left-full translate-y-3 translate-x-3",
+    "bottom-full left-full -translate-y-3 translate-x-3",
+  ];
 
   return (
     <div className="relative">
       {board}
-      <div className="absolute top-0 left-full opacity-30 translate-x-3">{board}</div>
-      <div className="absolute top-0 right-full opacity-30 -translate-x-3">{board}</div>
-      <div className="absolute bottom-full left-0 opacity-30 -translate-y-3">{board}</div>
-      <div className="absolute top-full left-0 opacity-30 translate-y-3">{board}</div>
-      <div className="absolute top-full right-full opacity-30 translate-y-3 -translate-x-3">{board}</div>
-      <div className="absolute bottom-full right-full opacity-30 -translate-y-3 -translate-x-3">{board}</div>
-      <div className="absolute top-full left-full opacity-30 translate-y-3 translate-x-3">{board}</div>
-      <div className="absolute bottom-full left-full opacity-30 -translate-y-3 translate-x-3">{board}</div>
+      {absoluteBoards.map((position, index) => (
+        <div
+          key={index}
+          className={`absolute ${position} transition-opacity duration-500 ${
+            index < visibleBoards ? "opacity-30" : "opacity-0"
+          }`}
+        >
+          {board}
+        </div>
+      ))}
     </div>
   );
 };
