91a880b0d0f6e1b6d99f4e85eb013b5402c2dac1
mraheja <mraheja@berkeley.edu>
2025-02-01 22:38:12 -0800

    extended grid

--- src/app/ultimate-tic-tac-torus/page.tsx
+++ src/app/ultimate-tic-tac-torus/page.tsx
@@ -2,7 +2,7 @@ import { UltimateTicTacTorus } from "@/components/molecules/UltimateTicTacTorus/
 
 const UltimateTicTacTorusPage = () => {
   return (
-    <div className="flex justify-center items-center min-h-[100dvh]">
+    <div className="flex justify-center items-center min-h-[100dvh] overflow-clip">
       <UltimateTicTacTorus />
     </div>
   );
--- src/components/molecules/UltimateTicTacTorus/UltimateTicTacTorus.tsx
+++ src/components/molecules/UltimateTicTacTorus/UltimateTicTacTorus.tsx
@@ -7,13 +7,30 @@ export interface UltimateTicTacTorusProps {
 export const UltimateTicTacTorus: React.FC<UltimateTicTacTorusProps> = ({
   className,
 }) => {
-  return (
+  const board = (
     <div className="grid grid-cols-3 gap-3 w-[500px] place-items-center">
-      {Array(9).fill(null).map((_, index) => (
-        <div className="border-2 border-black p-1" key={index}>
-            <TicTacToe/>
-        </div>
-      ))}
+      {Array(9)
+        .fill(null)
+        .map((_, index) => (
+          <div className="border-2 border-black p-1" key={index}>
+            <TicTacToe />
+          </div>
+        ))}
+    </div>
+  );
+
+
+  return (
+    <div className="relative">
+      {board}
+      <div className="absolute top-0 left-full opacity-30 translate-x-3">{board}</div>
+      <div className="absolute top-0 right-full opacity-30 -translate-x-3">{board}</div>
+      <div className="absolute bottom-full left-0 opacity-30 -translate-y-3">{board}</div>
+      <div className="absolute top-full left-0 opacity-30 translate-y-3">{board}</div>
+      <div className="absolute top-full right-full opacity-30 translate-y-3 -translate-x-3">{board}</div>
+      <div className="absolute bottom-full right-full opacity-30 -translate-y-3 -translate-x-3">{board}</div>
+      <div className="absolute top-full left-full opacity-30 translate-y-3 translate-x-3">{board}</div>
+      <div className="absolute bottom-full left-full opacity-30 -translate-y-3 translate-x-3">{board}</div>
     </div>
   );
 };
